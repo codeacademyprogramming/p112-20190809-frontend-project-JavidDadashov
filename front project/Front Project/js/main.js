d7ec7fce154c3be56b473178367d2d4c335e20f3
JavidDadashov <javidfd@code.edu.az>
2019-08-16 01:05:20 +0400

    main

---
+++ front project/Front Project/js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+$('.slide').owlCarousel({
+    loop: true,
+    dots: false,
+    nav: false,
+    navSpeed: 800,
+    autoplay: false,
+    transitionStyle: "fade",
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        }
+    }
+
+})
+
+
+
